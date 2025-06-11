4c5369c03b018de0aad42731d1245a72a299f5af
mahesh039 <burramaheshgoud2003@gmail.com>
2025-06-11 00:34:54 -0700

    Add files via upload

---
+++ sum of a listusing function.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+num1=float(input("enter the first number:"))
+num2=float(input("enter the second number:"))
+# perform arithmetic operations
+print("addition:",add(num1,num2))
+print("subtraction:",subtract(num1,num2))
+print("multuplication:",multiply(num1,num2))
+print("division:",divide(num1,num2))
